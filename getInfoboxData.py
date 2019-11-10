85f080c4c9f105dfd5af3703163bb19c9ed79a7c
mateusz <sulek.mat@gmail.com>
2019-11-10 10:25:53 +0100

    Adds lanuch.json, settings.json, requrements.txt.

--- getInfoboxData.py
+++ getInfoboxData.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
-import wptools as wp
+import requests as r
+import bs4 as bs
+
+url = 'https://en.wikipedia.org/wiki/Terry_Pratchett'
+
+req = r.get(url)
+
+print(req.text)
