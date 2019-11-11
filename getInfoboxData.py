949f54f9592117776c8fb140da78be7954694797
mateusz <sulek.mat@gmail.com>
2019-11-11 20:11:33 +0100

    test

--- getInfoboxData.py
+++ getInfoboxData.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
 import requests as r
-import bs4 as bs
+import bs4
 
-url = 'https://en.wikipedia.org/wiki/Terry_Pratchett'
+
+title = 'Terry_Pratchett' #'Arsène_Wenger'
+url = "http://en.wikipedia.org/wiki/{title}".format(title=title)
 
 req = r.get(url)
 
-print(req.text)
+
+
+soup = bs4.BeautifulSoup(req.text, 'lxml')
+
+infobox_table = soup.find('table', {'class': 'infobox vcard'}).findAll('tr')
+
+infobox = {}
+for row in infobox_table:
+    print(row.string)
+    for child in row.descendants:
+        for sibling in child.next_siblings:
+            print(child.string, " ; ", sibling.string)
