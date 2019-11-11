3c344b825e0cc7dab5942d5636882ebd0d9f1041
mateusz <sulek.mat@gmail.com>
2019-11-11 23:09:02 +0100

    trying with descendants

--- getInfoboxData.py
+++ getInfoboxData.py
@@ -4,7 +4,7 @@ import requests as r
 import bs4
 
 
-title = 'Terry_Pratchett' #'Arsène_Wenger'
+title = 'Arsène_Wenger' #Terry_Pratchett Arsène_Wenger Bill_Clinton
 url = "http://en.wikipedia.org/wiki/{title}".format(title=title)
 
 req = r.get(url)
@@ -17,7 +17,13 @@ infobox_table = soup.find('table', {'class': 'infobox vcard'}).findAll('tr')
 
 infobox = {}
 for row in infobox_table:
-    print(row.string)
-    for child in row.descendants:
-        for sibling in child.next_siblings:
-            print(child.string, " ; ", sibling.string)
+    if row.text:
+        print(row.name, " : ", row.text)
+        for child in row.children:
+            for sibling in child.next_siblings:
+                try:
+                    print(child.string, " ; ", sibling.text)
+                except AttributeError:
+                    continue
+    else:
+        continue
