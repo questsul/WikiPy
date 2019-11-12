7ed8dc2550dd1bb69f2e06a563cf8e3691200a1a
Mateusz Sulek <mateusz.sulek@airhelp.com>
2019-11-12 18:49:02 +0100

    final

--- getInfoboxData.py
+++ getInfoboxData.py
@@ -5,7 +5,7 @@ import bs4
 
 
 title = 'Terry_Pratchett' #Terry_Pratchett Arsène_Wenger Bill_Clinton
-url = "http://en.wikipedia.org/wiki/{title}".format(title=title)
+url = "https://en.wikipedia.org/wiki/{title}".format(title=title)
 
 req = r.get(url)
 
@@ -22,3 +22,22 @@ info = []
 for child in infobox_table:
     info.append(child.text.split('\n'))
 print(info)
+
+
+"Dwight_Howard",
+"Amelia_Earhart",
+"Harry_S_Truman",
+"Britney_Spears",
+"Paul_McCartney",
+"Maria_Sharapova",
+"Galileo_Galilei",
+"Eva_Longoria",
+"David_Copperfield",
+"Chuck_Lorre",
+"Peyton_Manning"
+"Diana_Princess_of_Wales",
+"Bill_Cosby",
+"Jerry_Seinfeld",
+"Elvis_Presley",
+"Heidi_Klum",
+"Aristotle",
